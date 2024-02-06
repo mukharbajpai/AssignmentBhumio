af7d0d48e864288d9a4e61f4521937dafe1936f6
mukharbajpai <mukharbajpai@gmail.com>
2024-02-06 13:14:57 +0530

    feat: base files added for controllers, tesseract, multer server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Hello This is a temporary Mock Comment
+require("dotenv").config();
+const express = require("express");
+const multer = require("multer");
+const { parseUploadedDocument } = require("./controllers/ocr.controller");
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 1667;
+const upload = multer({ dest: "uploads/" });
+
+app.post("/upload", upload.single("file"), parseUploadedDocument);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
