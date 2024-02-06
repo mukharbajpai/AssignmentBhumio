d4c82011cd3e08f4cc6513576d7e22627e902909
mukharbajpai <mukharbajpai@gmail.com>
2024-02-06 13:31:58 +0530

    Refactor parseUploadedDocument function and error handling

--- controllers/ocr.controller.js
+++ controllers/ocr.controller.js
@@ -1,20 +1,27 @@
 const ocr = require("../services/ocr.service");
 
+/**
+ * Parse the uploaded document, perform OCR on the image, and send the parsed response.
+ *
+ * @param {Object} req - the request object
+ * @param {Object} res - the response object
+ * @return {Promise} a Promise that resolves with the parsed response or rejects with an error
+ */
 const parseUploadedDocument = async (req, res) => {
   if (!req.file) {
-    res?.status(422)?.send("Please attach a file to file key");
+    return res.status(422).send("Please attach a file to file key");
   }
 
   try {
-    const imagePath = "uploads/" + req?.file?.filename;
+    const imagePath = "uploads/" + (req.file ? req.file.filename : '');
     const parsedResponse = await ocr(imagePath);
-    res?.status(200)?.json(parsedResponse);
+    return res.status(200).json(parsedResponse);
   } catch (error) {
-    console.error("Error in /upload endpoint:", error?.message);
-    res?.status(500)?.send("Internal Server Error!!");
+    console.error("Error in /upload endpoint:", error.message);
+    return res.status(500).send("Internal Server Error!!");
   }
 };
 
 module.exports = {
-  parseUploadedDocument: parseUploadedDocument,
+  parseUploadedDocument,
 };
